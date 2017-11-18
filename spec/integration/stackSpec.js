b5599e7cc2ca11c01ae2de6e2a7e981b29cd0609
Shekhar Patnaik <patnaikshekhar@gmail.com>
2017-11-18 20:35:15 +0000

    Added basic mongo template

--- spec/integration/stackSpec.js
+++ spec/integration/stackSpec.js
@@ -1,4 +1,5 @@
 const stack = require('../../lib/stack')
+const CONSTANTS = require('../../lib/constants')
 
 describe('stack', () => {
   describe('createStack', () => {
@@ -12,6 +13,21 @@ describe('stack', () => {
     it('should return an error when no file name is passed', (done) => {
       stack.createStack('test', null, (err) => {
         expect(err).not.toBe(null)
+        done()
+      })
+    })
+
+    it('should return an error if the template file doesnt exist', (done) => {
+      stack.createStack('test', `${__dirname}/../../templates/error.yml`, (err) => {
+        expect(err).not.toBe(null)
+        done()
+      })
+    })
+
+    it('should deploy the stack when template file is valid', (done) => {
+      stack.createStack('somthing', `${__dirname}/../../${CONSTANTS.SINGLE_NODE_TEMPLATE}`, (err, data) => {
+        expect(err).toBe(null)
+        expect(data).not.toBe(null)
         done()
       })
     })
